4de8c7291cbfa173e2763c4f6adcd02e073c4785
Felipe Valenzuela <feelipevalenzuela@hotmail.com>
2026-02-06 18:36:00 -0300

    Add Users views and view model, implementation of Create method

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication1.Models;
+using WebApplication1.Models.ViewModels;
 
 namespace WebApplication1.Controllers
 {
@@ -19,7 +22,46 @@ namespace WebApplication1.Controllers
             
             return View(await _context.Usuarios
                 .Include(u => u.UsuarioEmails)
+                .Include(u => u.Rol)
                 .ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Roles"] = new SelectList(_context.Roles, "RolId", "Nombre");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(UsuarioViewModel model)
+        {
+            
+
+            if (ModelState.IsValid)
+            {
+                var nuevoUsuario = new Usuario
+                {
+                    Nombre = model.Nombre,
+                    RolId = model.RolId,
+                    PasswordHash = model.Password, // Aquí deberías aplicar un hash real en lugar de almacenar la contraseña en texto plano
+                    FechaRegistro = DateTime.Now
+                };
+                
+
+
+                _context.Usuarios.Add(nuevoUsuario);
+                await _context.SaveChangesAsync();
+                var usuarioEmail = new UsuarioEmail
+                {
+                    UsuarioId = nuevoUsuario.UsuarioId,
+                    Email = model.Email,
+                    EsPrincipal = true
+                };
+                _context.UsuarioEmails.Add(usuarioEmail);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Roles"] = new SelectList(_context.Roles, "RolId", "Nombre", model.RolId);
+            return View(model);
         }
     }
 }
---
+++ Models/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display (Name = "Rol del Usuario")]
+        public int RolId { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
+
+
+    }
+}
